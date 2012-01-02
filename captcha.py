ca71fadd4226c0e7153c34fe995e84786148d6ae
Pedro Paulo <pedropaulovc@gmail.com>
2012-01-02 16:10:38 -0200

    Iniciando implementação do solvedor de CAPTCHA

---
+++ captcha.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 2, 2012
+
+@author: pedropaulovc
+'''
+
+class Captcha(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
